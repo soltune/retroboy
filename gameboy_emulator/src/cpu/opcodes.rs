f9795ee2a0ed6780db27d52d421b9d125cdbfb82
Samuel Parsons <samuel.m.parsons@gmail.com>
2023-11-08 07:41:49 -0500

    A few more load source register into dest opcodes

--- gameboy_emulator/src/cpu/opcodes.rs
+++ gameboy_emulator/src/cpu/opcodes.rs
@@ -11,16 +11,21 @@ pub enum Register {
     L
 }
 
-fn read_from_register(cpu_state: cpu::CpuState, register: Register) -> Result<u8, &'static str> {
+pub struct RegisterPair {
+    pub first: Register,
+    pub second: Register
+}
+
+fn read_from_register(cpu_state: &mut cpu::CpuState, register: Register) -> u8 {
     match register {
-        Register::A => Ok(cpu_state.registers.a),
-        Register::B => Ok(cpu_state.registers.b),
-        Register::C => Ok(cpu_state.registers.c),
-        Register::D => Ok(cpu_state.registers.d),
-        Register::E => Ok(cpu_state.registers.e),
-        Register::F => Ok(cpu_state.registers.f),
-        Register::H => Ok(cpu_state.registers.h),
-        Register::L => Ok(cpu_state.registers.l)
+        Register::A => cpu_state.registers.a,
+        Register::B => cpu_state.registers.b,
+        Register::C => cpu_state.registers.c,
+        Register::D => cpu_state.registers.d,
+        Register::E => cpu_state.registers.e,
+        Register::F => cpu_state.registers.f,
+        Register::H => cpu_state.registers.h,
+        Register::L => cpu_state.registers.l
     } 
 }
 
@@ -37,13 +42,41 @@ fn store_in_register(cpu_state: &mut cpu::CpuState, register: Register, value: u
     } 
 }
 
+fn read_from_register_pair(cpu_state: &mut cpu::CpuState, register_pair: RegisterPair) -> u16 {
+    let first_byte = read_from_register(cpu_state, register_pair.first);
+    let second_byte = read_from_register(cpu_state, register_pair.second);
+    ((first_byte as u16) << 8) | (second_byte as u16 & 0xFF)
+}
+
+fn store_in_register_pair(cpu_state: &mut cpu::CpuState, register_pair: RegisterPair, value: u16) {
+    store_in_register(cpu_state, register_pair.first, ((value >> 8) & 0xFF) as u8);
+    store_in_register(cpu_state, register_pair.second, (value & 0xFF) as u8);
+}   
+
 fn load_immediate_value(cpu_state: &mut cpu::CpuState, register: Register) {
     let immediate_byte = mmu::read_byte(&mut cpu_state.memory, cpu_state.registers.program_counter);
 
     store_in_register(cpu_state, register, immediate_byte);
-    
+
     cpu_state.registers.program_counter += 1;
     
+    cpu_state.clock.last_instr_clock_cycles = 8;
+    cpu_state.clock.last_instr_machine_cycles = 2;
+}
+
+fn load_source_register_in_destination_register(cpu_state: &mut cpu::CpuState, source: Register, destination: Register) {
+    let source_value = read_from_register(cpu_state, source);
+    store_in_register(cpu_state, destination, source_value);
+
+    cpu_state.clock.last_instr_clock_cycles = 4;
+    cpu_state.clock.last_instr_machine_cycles = 1;
+}
+
+fn load_memory_byte_in_destination_register(cpu_state: &mut cpu::CpuState, address_source_register_pair: RegisterPair, destination: Register) {
+    let address = read_from_register_pair(cpu_state, address_source_register_pair);
+    let byte = mmu::read_byte(&mut cpu_state.memory, address);
+    store_in_register(cpu_state, destination, byte);
+
     cpu_state.clock.last_instr_clock_cycles = 8;
     cpu_state.clock.last_instr_machine_cycles = 2;
 }
@@ -60,6 +93,9 @@ pub fn execute_opcode(cpu_state: &mut cpu::CpuState) {
         0x1e => load_immediate_value(cpu_state, Register::E),
         0x26 => load_immediate_value(cpu_state, Register::H),
         0x2e => load_immediate_value(cpu_state, Register::L),
+        0x78 => load_source_register_in_destination_register(cpu_state, Register::B, Register::A),
+        0x79 => load_source_register_in_destination_register(cpu_state, Register::C, Register::A),
+        0x7f => load_source_register_in_destination_register(cpu_state, Register::A, Register::A),
         _ => ()
     }
     
--- gameboy_emulator/src/cpu/opcodes/tests.rs
+++ gameboy_emulator/src/cpu/opcodes/tests.rs
@@ -2,11 +2,10 @@ use super::*;
 use crate::cpu;
 use crate::mmu;
 
-fn run_test_instructions(test_instructions: Vec<u8>) -> cpu::CpuState {
+fn init_cpu_with_test_instructions(test_instructions: Vec<u8>) -> cpu::CpuState {
     let mut cpu_state = cpu::initialize_cpu_state();
     cpu_state.memory.in_bios = false;
     mmu::load_rom_buffer(&mut cpu_state.memory, test_instructions);
-    execute_opcode(&mut cpu_state);
     cpu_state
 }
 
@@ -17,7 +16,8 @@ fn assert_cycles(cpu_state: &cpu::CpuState, machine_cycles: u8) {
 
 #[test]
 fn loads_immediate_byte_into_register_b() {
-    let cpu_state = run_test_instructions(vec![0x06, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x06, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.b, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
@@ -25,7 +25,8 @@ fn loads_immediate_byte_into_register_b() {
 
 #[test]
 fn loads_immediate_byte_into_register_c() {
-    let cpu_state = run_test_instructions(vec![0x0e, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x0e, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.c, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
@@ -33,7 +34,8 @@ fn loads_immediate_byte_into_register_c() {
 
 #[test]
 fn loads_immediate_byte_into_register_d() {
-    let cpu_state = run_test_instructions(vec![0x16, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x16, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.d, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
@@ -41,7 +43,8 @@ fn loads_immediate_byte_into_register_d() {
 
 #[test]
 fn loads_immediate_byte_into_register_e() {
-    let cpu_state = run_test_instructions(vec![0x1e, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x1e, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.e, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
@@ -49,7 +52,8 @@ fn loads_immediate_byte_into_register_e() {
 
 #[test]
 fn loads_immediate_byte_into_register_h() {
-    let cpu_state = run_test_instructions(vec![0x26, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x26, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.h, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
@@ -57,8 +61,41 @@ fn loads_immediate_byte_into_register_h() {
 
 #[test]
 fn loads_immediate_byte_into_register_l() {
-    let cpu_state = run_test_instructions(vec![0x2e, 0xA1]);
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x2e, 0xA1]);
+    execute_opcode(&mut cpu_state);
     assert_eq!(cpu_state.registers.l, 0xA1);
     assert_eq!(cpu_state.registers.program_counter, 2);
     assert_cycles(&cpu_state, 2)
+}
+
+#[test]
+fn loads_register_a_into_register_a() {
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x7f]);
+    cpu_state.registers.a = 0x88;
+    execute_opcode(&mut cpu_state);
+    assert_eq!(cpu_state.registers.a, 0x88);
+    assert_eq!(cpu_state.registers.program_counter, 1);
+    assert_cycles(&cpu_state, 1)
+}
+
+#[test]
+fn loads_register_b_into_register_a() {
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x78]);
+    cpu_state.registers.b = 0x2F;
+    cpu_state.registers.a = 0x88;
+    execute_opcode(&mut cpu_state);
+    assert_eq!(cpu_state.registers.a, 0x2F);
+    assert_eq!(cpu_state.registers.program_counter, 1);
+    assert_cycles(&cpu_state, 1)
+}
+
+#[test]
+fn loads_register_c_into_register_a() {
+    let mut cpu_state = init_cpu_with_test_instructions(vec![0x79]);
+    cpu_state.registers.c = 0x2F;
+    cpu_state.registers.a = 0x88;
+    execute_opcode(&mut cpu_state);
+    assert_eq!(cpu_state.registers.a, 0x2F);
+    assert_eq!(cpu_state.registers.program_counter, 1);
+    assert_cycles(&cpu_state, 1)
 }
