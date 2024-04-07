f9f8199bd8722f6ab92241d2b993b64e84691798
Samuel Parsons <samuel.m.parsons@gmail.com>
2024-04-07 14:43:20 -0400

    No need for period

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn open_window_and_render_pixel() {
 fn main() -> io::Result<()> {
     let filepath = "/Users/samuelparsons/development/gb-test-roms/cpu_instrs/cpu_instrs.gb";
     let emulator = initialize_emulator_by_filepath(filepath)
-        .expect("An error occurred when trying to load the ROM.");
+        .expect("An error occurred when trying to load the ROM");
 
     println!("{:?} {:?} {:?}", emulator.cpu.registers, emulator.cpu.clock, emulator.memory.rom);
     
