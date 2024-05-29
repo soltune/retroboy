89ec39e7320f433adfb341b64725ebebd4a9f0bf
Samuel Parsons <samuel.m.parsons@gmail.com>
2024-05-29 07:53:44 -0400

    APU - logic for wave duty position

--- src/apu/tests.rs
+++ src/apu/tests.rs
@@ -4,32 +4,66 @@ use super::*;
 #[test]
 fn should_decrement_period_divider() {
     let mut emulator = initialize_emulator();
-    emulator.apu.ch1_period_divider = 0b010100011010;
-    emulator.apu.ch1_period_low = 0b00011010;
-    emulator.apu.ch1_period_high = 0b11000101;
+    emulator.apu.ch1_period_divider = 742;
+    emulator.apu.ch1_period_low = 26;
+    emulator.apu.ch1_period_high = 197;
     emulator.cpu.clock.instruction_clock_cycles = 4;
     step(&mut emulator);
-    assert_eq!(emulator.apu.ch1_period_divider, 0b010100011001);
+    assert_eq!(emulator.apu.ch1_period_divider, 741);
 }
 
 #[test]
 fn should_decrement_period_divider_twice_with_eight_instruction_cycles() {
     let mut emulator = initialize_emulator();
-    emulator.apu.ch1_period_divider = 0b010100011010;
-    emulator.apu.ch1_period_low = 0b00011010;
-    emulator.apu.ch1_period_high = 0b11000101;
+    emulator.apu.ch1_period_divider = 742;
+    emulator.apu.ch1_period_low = 26;
+    emulator.apu.ch1_period_high = 197;
     emulator.cpu.clock.instruction_clock_cycles = 8;
     step(&mut emulator);
-    assert_eq!(emulator.apu.ch1_period_divider, 0b010100011000); 
+    assert_eq!(emulator.apu.ch1_period_divider, 740); 
 }
 
 #[test]
 fn should_reload_period_divider_once_it_reaches_zero() {
    let mut emulator = initialize_emulator();
-   emulator.apu.ch1_period_divider = 0b1;
-   emulator.apu.ch1_period_low = 0b00011010;
-   emulator.apu.ch1_period_high = 0b11000101;
+   emulator.apu.ch1_period_divider = 1;
+   emulator.apu.ch1_period_low = 26;
+   emulator.apu.ch1_period_high = 197;
    emulator.cpu.clock.instruction_clock_cycles = 4;
    step(&mut emulator);
-   assert_eq!(emulator.apu.ch1_period_divider, 0b10100011010);
+   assert_eq!(emulator.apu.ch1_period_divider, 742);
+}
+
+#[test]
+fn should_properly_wrap_period_divider_value_with_eight_instruction_cycles() {
+    let mut emulator = initialize_emulator();
+    emulator.apu.ch1_period_divider = 1;
+    emulator.apu.ch1_period_low = 26;
+    emulator.apu.ch1_period_high = 197;
+    emulator.cpu.clock.instruction_clock_cycles = 8;
+    step(&mut emulator);
+    assert_eq!(emulator.apu.ch1_period_divider, 741);  
+}
+
+#[test]
+fn should_increment_wave_duty_position_when_period_divider_reaches_zero() {
+    let mut emulator = initialize_emulator();
+    emulator.apu.ch1_period_divider = 1;
+    emulator.apu.ch1_period_low = 26;
+    emulator.apu.ch1_period_high = 197;
+    emulator.cpu.clock.instruction_clock_cycles = 4;
+    step(&mut emulator);
+    assert_eq!(emulator.apu.ch1_wave_duty_position, 1);
+}
+
+#[test]
+fn should_reset_wave_duty_position_to_zero_when_increased_above_seven() {
+    let mut emulator = initialize_emulator();
+    emulator.apu.ch1_period_divider = 1;
+    emulator.apu.ch1_period_low = 26;
+    emulator.apu.ch1_period_high = 197;
+    emulator.apu.ch1_wave_duty_position = 7;
+    emulator.cpu.clock.instruction_clock_cycles = 4;
+    step(&mut emulator);
+    assert_eq!(emulator.apu.ch1_wave_duty_position, 0);
 }
