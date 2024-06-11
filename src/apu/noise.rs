f8cdc7626315bc40a19e1f69a2550c9876c9b08d
Samuel Parsons <samuel.m.parsons@gmail.com>
2024-06-10 21:02:44 -0400

    Simpler way to compute divisor

--- src/apu/noise.rs
+++ src/apu/noise.rs
@@ -33,24 +33,15 @@ pub fn initialize_noise_channel() -> NoiseChannel {
 // divider every 16 T-cycles.
 const PERIOD_DIVIDER_RATE_IN_INSTRUCTION_CYCLES: u8 = 16;
 
-fn as_divisor(divisor_code: u8) -> u16 {
-    match divisor_code {
-        0 => 8,
-        1 => 16,
-        2 => 32,
-        3 => 48,
-        4 => 64,
-        5 => 80,
-        6 => 96,
-        7 => 112,
-        _ => panic!("Invalid divisor code.")
-    }
-}
-
 fn calculate_period_divider(channel: &NoiseChannel) -> u16 {
     let shift_amount = (channel.polynomial & 0b11110000) >> 4;
     let divisor_code = channel.polynomial & 0b111;
-    let divisor = as_divisor(divisor_code);
+    let divisor = if divisor_code == 0 {
+        8
+    }
+    else {
+        (divisor_code as u16) << 4
+    };
     divisor << shift_amount
 }
 
